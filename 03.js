215615cbcffccc4ac960ebf458b4a95ce73ad3b3
yeonju0318 <cuteyonju@gmail.com>
2023-04-05 22:11:53 +0900

    조건문 복습ing

---
+++ 03.js
@@ -0,0 +1,23 @@
+//조건문 복습
+
+//조건문은 if로 시작한다. if 뒤의 괄호에 조건이 오고, 조건이 될 수 있는 값는 Boolean이다. Boolean의 값이 true라면 조건이 담겨진 괄호 다음의 중괄호 구문이 실행된다.
+
+//else는 주어진 조건이 거짓일 때 실행할 구간을 정의하는 것이다.
+
+//else if는 좀 더 다양한 케이스의 조건을 검사할 수 있는 기회를 제공한다.
+
+//else if의 모든 조건이 false라면 else가 실행된다. else는 생략 가능하다.
+
+if(false){
+    alert(1);
+} else if(false){
+    alert(2);
+} else if(false){
+    alert(3);
+} else {
+    alert(4);
+}
+
+
+//&&는 좌항과 우항이 모두 참(true)일 때 참이된다.
+
